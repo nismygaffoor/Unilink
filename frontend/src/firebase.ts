5f8aafe14d1c27e0ad44892523d617be784fb54a
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-14 23:53:32 +0530

    Create firebase.ts

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
+// import { getStorage } from 'firebase/storage';
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyD-wDyzvuEXLcOuEC3LvPvKASR6C5iwcz8",
+  authDomain: "unilink-ffb98.firebaseapp.com",
+  projectId: "unilink-ffb98",
+  storageBucket: "unilink-ffb98.firebasestorage.app",
+  messagingSenderId: "122824338280",
+  appId: "1:122824338280:web:ac348afc3484f769a844dd",
+  measurementId: "G-1P5H3ZG87R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+export const auth = getAuth(app);
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
+
+export default app;
