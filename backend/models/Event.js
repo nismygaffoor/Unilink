4bc5a581831ac52bdf9e08cb90246d26f2bd042f
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-14 23:06:11 +0530

    Create Event.js

---
+++ backend/models/Event.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  description: { type: String }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', EventSchema);
