0e6786bed29a28c28a8b57a663541cd6d6ce3951
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-14 23:06:04 +0530

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+
+// Get all users (leaderboard)
+exports.getLeaderboard = async (req, res) => {
+  try {
+    const users = await User.find().sort({ points: -1 });
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Get user profile by email
+exports.getProfile = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
