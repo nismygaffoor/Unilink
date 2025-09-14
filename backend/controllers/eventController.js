691f625d77a0c150143a2fc199af2ea6ad8bbb16
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-14 23:05:52 +0530

    Create eventController.js

---
+++ backend/controllers/eventController.js
@@ -0,0 +1,22 @@
+const Event = require('../models/Event');
+
+// Get all events
+exports.getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Create a new event
+exports.createEvent = async (req, res) => {
+  try {
+    const event = new Event(req.body);
+    await event.save();
+    res.status(201).json(event);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
