83d9640307066fae3a5018ce7469293e1eb319c3
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-14 23:05:56 +0530

    Create noteController.js

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,22 @@
+const Note = require('../models/Note');
+
+// Get all notes
+exports.getNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Create a new note
+exports.createNote = async (req, res) => {
+  try {
+    const note = new Note(req.body);
+    await note.save();
+    res.status(201).json(note);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
