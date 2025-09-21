bdf8cb9453eda15199d80292f8779f65e906b355
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-21 13:35:44 +0530

    Update Note.js

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  subject: { type: String, required: true },   // e.g. "Mathematics"
+  course: { type: String, required: true },    // e.g. "BSc Computer Science"
+  unit: { type: String, required: true },      // e.g. "Algebra I"
   description: { type: String },
-  uploader: { type: String, required: true }, // Firebase UID
+  uploader: { type: String, required: true },
   link: { type: String, required: true }
 }, { timestamps: true });
+
 
 module.exports = mongoose.model('Note', NoteSchema);
