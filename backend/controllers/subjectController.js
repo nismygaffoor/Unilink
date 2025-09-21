afd6951507dc2a65cb5a0c68292ac9bdc7239c26
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-22 02:26:25 +0530

    Create subjectController.js

---
+++ backend/controllers/subjectController.js
@@ -0,0 +1,22 @@
+const Subject = require('../models/Subject');
+
+// Get all subjects
+exports.getSubjects = async (req, res) => {
+  try {
+    const subjects = await Subject.find();
+    res.json(subjects);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Create a new subject
+exports.createSubject = async (req, res) => {
+  try {
+    const subject = new Subject(req.body);
+    await subject.save();
+    res.status(201).json(subject);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
