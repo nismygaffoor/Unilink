e12cc3163bae0f768c324b119d0be9969d9225a5
nismygaffoor <nismygaffoor5@gmail.com>
2025-09-21 13:35:48 +0530

    Update noteRoutes.js

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,11 +1,23 @@
+// routes/noteRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getNotes, createNote } = require('../controllers/noteController');
+const {
+  getNotes,
+  createNote,
+  getSubjects,
+  getNotesBySubject
+} = require('../controllers/noteController');
 
 // Get all notes
 router.get('/', getNotes);
 
 // Create a new note
 router.post('/', createNote);
+
+// Get all subjects
+router.get('/subjects', getSubjects);
+
+// Get notes by subject
+router.get('/:subject', getNotesBySubject);
 
 module.exports = router;
