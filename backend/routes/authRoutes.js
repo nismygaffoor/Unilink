bb767e36f24fc5e1ca1b4b03e19805f15207574b
nismygaffoor <nismygaffoor5@gmail.com>
2025-10-20 23:17:56 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
-const express = require('express');
+// backend/routes/auth.js
+const express = require("express");
+const { ensureProfile } = require("../controllers/authController");
 const router = express.Router();
-const { signup } = require('../controllers/authController');
 
-// Signup route
-router.post('/signup', signup);
+// POST /api/auth/ensureProfile
+router.post("/ensureProfile", ensureProfile);
 
 module.exports = router;
